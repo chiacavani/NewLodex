ea1e3e891a96aeca5ac8842a7cd5c310e7823bae
Chiara <chicca4ever96@live.it>
2018-05-16 12:34:21 +0200

    ok

--- lodex2_webapp/server.py
+++ lodex2_webapp/server.py
@@ -13,15 +13,24 @@ from tornado import gen
 
 exclusion = []
 
+# VISUALIZZA PRIMA PAGINA
 class MainHandlerOk(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.render("LODeX.html")
 
+
+# VISULIZZA PAGINA GRAPH
 class GraphView(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.render("graph-view.html")
+
+#VISUALIZZA PAGINA QUERY
+class QueryView(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.render("query-panel.html")
 
 
 if __name__ == "__main__":
@@ -29,7 +38,8 @@ if __name__ == "__main__":
     db2 = motor.MotorClient().lodex
     application = tornado.web.Application(handlers=[
         (r"/lodex2/ok", MainHandlerOk),
-        (r"/lodex2/ok/graph-view", GraphView)],
+        (r"/lodex2/ok/graph-view", GraphView),
+        (r"/lodex2/ok/query-panel", QueryView)],
          static_path=os.path.join(os.path.dirname(__file__), "static"), db=db)
     port = 8891
     print('Listening on http://localhost:', port)
